522a004822dfb9c4e96bc7de73a6e77790e374b8
Farzona <farzonarakh@users.noreply.github.com>
2025-09-15 09:28:36 +0500

    Alternate placing 'X's and 'O's

--- tic-tac-toe/src/App.jsx
+++ tic-tac-toe/src/App.jsx
@@ -1,16 +1,10 @@
 import { useState } from "react";
 
-function Square() {
-  const [value, setvalue] = useState(null);
-
-  function handleClick() {
-    setvalue('X')
-  }
-
+function Square({value, onSquareClick}) {
   return (
     <button
       className="square"
-      onClick={handleClick}
+      onClick={onSquareClick}
     >
       {value}
     </button>
@@ -18,22 +12,39 @@ function Square() {
 }
 
 export default function Board() {
+  const [xIsNext, setIsNext] = useState(true)
+  const [squares, setSquares] = useState(Array(9).fill(null));
+
+  function handleClick(i) {
+    if(squares[i]) {
+      return;
+    }
+    const nextSquares = squares.slice();
+    if (xIsNext) {
+      nextSquares[i] = "X";
+    } else {
+      nextSquares[i] = "O";
+    }
+    setSquares(nextSquares);
+    setIsNext(!xIsNext);
+  }
+
   return (
     <>
       <div className="board-row">
-        <Square />
-        <Square />
-        <Square />
+        <Square value={squares[0]} onSquareClick={() => {handleClick(0)}} />
+        <Square value={squares[1]} onSquareClick={() => {handleClick(1)}} />
+        <Square value={squares[2]} onSquareClick={() => {handleClick(2)}} />
       </div>
       <div className="board-row">
-        <Square />
-        <Square />
-        <Square />
+        <Square value={squares[3]} onSquareClick={() => {handleClick(3)}} />
+        <Square value={squares[4]} onSquareClick={() => {handleClick(4)}} />
+        <Square value={squares[5]} onSquareClick={() => {handleClick(5)}} />
       </div>
       <div className="board-row">
-        <Square />
-        <Square />
-        <Square />
+        <Square value={squares[6]} onSquareClick={() => {handleClick(6)}}/>
+        <Square value={squares[7]} onSquareClick={() => {handleClick(7)}}/>
+        <Square value={squares[8]} onSquareClick={() => {handleClick(8)}} />
       </div>
     </>
   );
